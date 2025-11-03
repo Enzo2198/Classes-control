84b42f7845e1f7cd775ed1dbd57208bbc664de76
vgspc <gianggvf8209@fullstack.edu.vn>
2025-11-03 13:05:16 +0700

    update

--- api/src/main.ts
+++ api/src/main.ts
@@ -31,6 +31,6 @@ async function bootstrap() {
     credentials: true, // use authentication
   });
 
-  await app.listen(process.env.API_PORT ?? 3000);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
