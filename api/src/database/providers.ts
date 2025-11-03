ee3bc8a34df861cb014adce5db105d7f8d00ec26
vgspc <gianggvf8209@fullstack.edu.vn>
2025-11-03 14:59:25 +0700

    update

--- api/src/database/providers.ts
+++ api/src/database/providers.ts
@@ -26,8 +26,8 @@ export const databaseProviders = [
     useFactory: async () => {
       const dataSource = new DataSource({
         type: 'postgres',
-        host: 'db',
-        port: 5432,
+        host: process.env.DB_HOST,
+        port: parseInt(process.env.DB_PORT ?? '5432'),
         username: process.env.DB_USERNAME,
         password: process.env.DB_PASSWORD,
         database: process.env.DB_NAME,
